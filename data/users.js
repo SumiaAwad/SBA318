0ef21dc843b603eab4519ddd049fb33f1453bebd
SumiaAwad <159062215+SumiaAwad@users.noreply.github.com>
2024-04-12 17:22:56 -0400

    final commit

--- data/users.js
+++ data/users.js
@@ -1,70 +1,54 @@
 const users = [
     {
         id: 1,
-        name: "John Doe",
-        username: "John23",
-        email: "John23@example.com",
+        name: "Ahmed Awad",
+        username: "Ahmed90",
+        email: "Ahmed90@example.com",
 
     },
 
 
     {
         id: 2,
-        name: "Jane Smith",
-        username: "John23",
-        email: "Jane22@example.com",
+        name: "Amina Hamd",
+        username: "Amina21",
+        email: "Amina21@example.com",
 
     },
 
     {
         id: 3,
-        name: "Christin Brook",
-        username: "Chiris33",
-        email: "Chris33@example.com",
+        name: "Jordan Mike",
+        username: "mike3",
+        email: "mike3@example.com",
 
     },
 
 
     {
         id: 4,
-        name: "Graham  Moris",
-        username: "Graham44",
-        email: "Graham44@example.com"
+        name: "Marhaba Awad",
+        username: "Awad91",
+        email: "Awad91@example.com"
     },
 
     {
         id: 5,
-        name: "Cozy  Balard",
-        username: "Cozy55",
-        email: "Cozy55@example.com",
+        name: "Roro Kbosh",
+        username: "Kbosh28",
+        email: "Kbosh28@example.com",
 
     },
 
 
     {
         id: 6,
-        name: "Keith Hunting",
-        username: "Keith66",
-        email: "Keith66@example.com",
+        name: "Moaz Awad",
+        username: "Moaz10",
+        email: "Moaz10@example.com",
 
     },
 
-    {
-        id: 7,
-        name: "Jacky Maher",
-        username: "Jacky77",
-        email: "Jacky77@example.com",
-
-    },
-
-
-    {
-        id: 8,
-        name: "Jonny Boe",
-        username: "Jonny88",
-        email: "Jonny88@example.com",
-
-    },
 
 
 ]
